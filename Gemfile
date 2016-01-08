ebdf4523fe95c23c910c0477e0886efcc4d794bc
AzDarGee <ashishdarji88@gmail.com>
2016-01-07 22:28:10 -0500

    added script to load data from kevins API and input it into the database

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'omniauth'
 gem 'omniauth-digitalocean'
 gem 'omniauth-facebook'
 gem 'ratyrate'
+gem 'unirest'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'unirest'
+
+def get_dev_ids(url)
+  response = Unirest.get(url)
+
+  f = File.new("dev_ids_Jan_7_2015", "w+")
+  response.body.each_with_index do |dev, index|
+    f.write(dev['devid'] + "\n")
+  end
+  f.close
+end
+
+def create_dev_apps_file(list_of_dev_ids)
+  f = File.new("devapp_endpoints_Jan_7_2015", "w+")
+  ids = File.open(list_of_dev_ids).read
+  ids.each_line do |id|
+    if (id != "" || id != nil)
+        f.write("http://ottwatch.ca/api/devapps/"+id)
+    end
+  end
+  f.close
+end
+
+def get_data(endpoints_list)
+  endpoints = File.open(endpoints_list).read
+
+  counter = 0
+  endpoints.each_line do |url|
+    dev = DevSite.new
+    response = Unirest.get(url)
+    dev.devID = response.body["devid"]
+    dev.application_type = response.body["apptype"]
+    dev.ward_num = response.body["ward"]
+    dev.description = response.body["description"]
+    counter += 1
+    puts counter
+    dev.save
+  end
+
+end
+
+# url = "http://ottwatch.ca/api/devapps/all"
+# get_dev_ids(url)
+# create_dev_apps_file("dev_ids_Jan_7_2015")
+get_data("db/devapp_endpoints_Jan_7_2015")
