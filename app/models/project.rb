2663e158e0d38db173c7c9fd0ca68bde1f76cd26
AzDarGee <ashishdarji88@gmail.com>
2016-04-13 22:57:37 -0400

    projects model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,12 @@
 class Project < ActiveRecord::Base
+  attr_accessor :images, :files
+
+  # Voting
+  acts_as_votable
+
+  # CarrierWave - Images
+  mount_uploaders :images, ImagesUploader
+
+  # CarrierWave - Files
+  mount_uploaders :files, FilesUploader
 end
