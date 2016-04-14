86ac73a5559a1cbc6f0aeda9040a46dc37c08ddb
AzDarGee <ashishdarji88@gmail.com>
2016-04-13 23:09:10 -0400

    project crud - controller and model updates

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,53 @@
 class ProjectsController < ApplicationController
+before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
+    @projects = Project.all
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 
   def new
+    @project = Project.new
   end
 
   def edit
+    @project = Project.find(params[:id])
   end
 
   def update
+    if @dev_site.update(project_params)
+      redirect_to @project, notice: 'Dev site was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @project.destroy
+    redirect_to projects_index_path, notice: "Project was deleted."
   end
 
   def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: 'Project was successfully created.'
+    else
+      render :new
+    end
   end
+
+private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:dev_site).permit(:title, :description, :funding_goal, :funds_raised)
+  end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -7,6 +7,12 @@ class Project < ActiveRecord::Base
   # Voting
   acts_as_votable
 
+  # Validations
+  validates     :title, presence: { message: "A project title is required" }
+  validates     :description, presence: { message: "A project description is required" }
+  validates     :funds_raised, numericality: true
+  validates     :funding_goal, presence: { message: "Funding goal is required" }, numericality: true
+
   # CarrierWave - Images
   mount_uploaders :images, ImagesUploader
 
