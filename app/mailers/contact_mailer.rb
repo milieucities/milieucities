7850e2a89b43feea451120a9a1fdddb4a1a8177d
mtuck063 <meneliktucker@gmail.com>
2016-04-26 13:27:15 -0400

    Email info@milieu.io for contact form

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
 
   def contact_citizencity(name, email, message)
     @name, @email, @message = name, email, message
-    mail(:to => 'meneliktucker@hotmail.com', :subject => "Email from Citizen City contact form")
+    mail(:to => 'info@milieu.io', :subject => "Citizen City contact form")
   end
 
 end
