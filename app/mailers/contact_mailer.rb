36072688669185ae4ed21ee4a07d36e9a3dd3928
mtuck063 <meneliktucker@gmail.com>
2016-05-06 09:38:47 -0400

    Test if you can send email to self

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,17 +2,17 @@ class ContactMailer < ApplicationMailer
 
   def contact_citizencity(name, email, message)
     @name, @email, @message = name, email, message
-    mail(:to => 'meneliktucker@hotmail.com', :subject => "Citizen City contact form")
+    mail(:to => 'meneliktucker@gmail.com', :subject => "Citizen City contact form")
   end
 
   def contact_milieu(name, email, message)
     @name, @email, @message = name, email, message
-    mail(:to => 'meneliktucker@hotmail.com', :subject => "Milieu contact form")
+    mail(:to => 'meneliktucker@gmail.com', :subject => "Milieu contact form")
   end
 
   def contact_file_lead(name, email, message)
     @name, @email, @message = name, email, message
-    mail(:to => 'meneliktucker@hotmail.com', :subject => "Message for File Lead")
+    mail(:to => 'meneliktucker@gmail.com', :subject => "Message for File Lead")
   end
 
 end
