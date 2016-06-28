8dfc4fb952c9734269f012c0bfc615eed2baf9a6
Alex Kluew <dropbyalex@gmail.com>
2016-06-28 19:12:05 -0400

    Started DevSite controller tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :destroy]
 
   def index
     @users = User.all
