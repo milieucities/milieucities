03d32f4c31dd4840cc559640b164d54dda761146
Alex Kluew <dropbyalex@gmail.com>
2016-06-28 18:41:15 -0400

    Added tests for dev_site model

---
+++ spec/factories/dev_site.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :dev_site do
+    devID { FFaker::Identification.drivers_license }
+    application_type "Site Plan Control"
+    description "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    ward_name { FFaker::Name }
+    ward_num 13
+  end
+end
