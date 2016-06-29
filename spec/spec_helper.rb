325efadfecd7a04ea9264f6d228deb6d538901fb
Alex Kluew <dropbyalex@gmail.com>
2016-06-28 23:16:13 -0400

    Adding other gems to fix codeship

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 require 'factory_girl_rails'
+require 'ffaker'
+require 'shoulda-matchers'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
