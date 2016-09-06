4e59e7aa188aaffeba4546be9d67b37091131a07
mtuck063 <meneliktucker@gmail.com>
2016-09-06 17:48:19 -0400

    update facebook link

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -2,13 +2,17 @@ class OmniauthController < ApplicationController
 
   def create
     @user = User.find_by(uid: request.env['omniauth.auth']['uid'])
-    
+
     unless @user.present?
       @user = User.create(uid: request.env['omniauth.auth']['uid'],
+                          email: request.env['omniauth.auth']['info']['email'],
                           provider: request.env['omniauth.auth']['provider'])
       @user.build_profile(name: request.env['omniauth.auth']['info']['name'])
       @user.save
     end
+
+    @user.update(email: request.env['omniauth.auth']['info']['email']) if @user.email.nil?
+
     session[:user_id]= @user.id
     redirect_to root_path, notice: "Welcome to Milieu"
   end
