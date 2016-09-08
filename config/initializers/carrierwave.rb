035909e978b421acf2f6dcbb2684560fddc033dd
Ilie Diacov <iliadiacov@mail.ru>
2016-09-07 20:25:36 -0400

    reverting to 4e59e7a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider              => 'AWS',
-    :aws_access_key_id     => ENV['S3_KEY'] = "splice",
-    :aws_secret_access_key => ENV['S3_SECRET'] = "splice",
+    :aws_access_key_id     => ENV['S3_KEY'],
+    :aws_secret_access_key => ENV['S3_SECRET'],
     :region                => ENV['S3_REGION']
   }
 
