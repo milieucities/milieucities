11f385c7601f07e01691ff2e8eab4fdc263d7f40
Sharon <sharon.peishan.kennedy@gmail.com>
2017-01-04 12:58:45 +0100

    fixed rubocop errors in mailers

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,23 +1,29 @@
 class ContactMailer < ApplicationMailer
-
   def contact_citizencity(name, email, message)
-    @name, @email, @message = name, email, message
-    mail(:to => 'info@milieu.io', :subject => "Citizen City contact form")
+    @name = name
+    @email = email
+    @message = message
+    mail(to: MILIEU_EMAIL_ADDRESS, subject: 'Citizen City contact form')
   end
 
   def contact_milieu(message)
     @name, @email, @message = message.values
-    mail(:to => 'info@milieu.io', :subject => "Milieu contact form")
+    mail(to: MILIEU_EMAIL_ADDRESS, subject: 'Milieu contact form')
   end
 
   def contact_file_lead(message)
     @name, @email, @message, @dev_site_id = message.values
-    mail(:to => [DevSite.find(@dev_site_id).urban_planner_email], :subject => "Message for File Lead from Milieu.io")
+    email_to = DevSite.find(@dev_site_id).urban_planner_email
+    subject = 'Message for File Lead from Milieu.io'
+
+    mail(to: [email_to], subject: subject)
   end
 
   def contact_councillor(message)
     @name, @email, @message, @dev_site_id = message.values
-    mail(:to => [DevSite.find(@dev_site_id).ward_councillor_email], :subject => "Message for Councillor from Milieu.io")
-  end
+    email_to = DevSite.find(@dev_site_id).ward_councillor_email
+    subject = 'Message for Councillor from Milieu.io'
 
+    mail(to: [email_to], subject: subject)
+  end
 end
