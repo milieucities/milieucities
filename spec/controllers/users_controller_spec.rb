890f57c962a33c5092cb7504951e659f99e44bce
Sharon <sharon.peishan.kennedy@gmail.com>
2017-01-14 20:24:42 -0500

    user controller tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,119 @@
+require 'spec_helper'
+
+describe UsersController do
+  context 'user is authorized' do
+    let(:user) { create(:user) }
+    let(:admin_user) { create(:admin_user) }
+
+    describe '#index' do
+      it 'should return all users as json' do
+        sign_in admin_user
+
+        get :index, format: :json
+
+        expect(response.status).to eq(200)
+        expect(response.content_type).to eq('application/json')
+        expect(JSON.parse(response.body)[0]['id']).to eq(admin_user.id)
+      end
+    end
+
+    describe '#new' do
+      it 'should build profile for user' do
+        get :new
+
+        expect(assigns(:user).profile).to_not be_nil
+      end
+    end
+
+    describe '#show' do
+      it 'should assign no_header to true' do
+        sign_in user
+
+        get :show, { slug: user.slug }, format: :json
+
+        expect(assigns(:no_header)).to be true
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe '#edit' do
+      it 'should assign no_header to true' do
+        sign_in user
+
+        get :edit, { slug: user.slug }, format: :json
+
+        expect(assigns(:no_header)).to be true
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe '#create' do
+      before(:each) do
+        @user_attrs = attributes_for(:user)
+      end
+
+      it 'should set a session and redirect to the root path when user is saved' do
+        post :create, { user: @user_attrs }, format: :json
+
+        expect(session[:user_id]).to eq(assigns(:user).id)
+        expect(response).to redirect_to(root_path)
+      end
+
+      it 'should flash errors and render the new template when user is not saved' do
+        post :create, { user: @user_attrs.except(:email) }, format: :json
+
+        expect(flash[:alert]).to eq('Email is required')
+        expect(response).to render_template(:new)
+      end
+    end
+
+    describe '#update' do
+      before(:each) do
+        sign_in user
+      end
+
+      it 'should render show if user updates' do
+        user_attrs = { email: 'test@test.com' }
+
+        patch :update, { slug: user.slug, user: user_attrs }, format: :json
+
+        expect(assigns(:user).email).to eq(user_attrs[:email])
+        expect(response).to render_template(:show)
+      end
+
+      it 'should return errors as json if user fails to update' do
+        invalid_attrs = { email: '' }
+
+        patch :update, { slug: user.slug, user: invalid_attrs }, format: :json
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    describe '#destroy' do
+      it 'should delete current user and session' do
+        sign_in user
+
+        delete :destroy, { slug: user.slug }, format: :json
+        expect(session[:user_id]).to be_nil
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  context 'user is not authorized' do
+    let(:user) { create(:user) }
+
+    describe '#show, #edit, #update, #destroy' do
+      it 'should redirect to root path if user is not authorized' do
+        restricted_actions = [:show, :edit, :update, :destroy]
+        restricted_actions.each do |action|
+          get action, slug: user.slug
+
+          expect(flash[:alert]).to eq('You are not authorized to access this page.')
+          expect(response).to redirect_to(root_path)
+        end
+      end
+    end
+  end
+end
