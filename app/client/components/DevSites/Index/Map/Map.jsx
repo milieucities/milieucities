40a6985b0f64fdeae8ebcc87c4391c3b60b7b4ca
LuYao Ji <strawberryfield@LuYaos-MacBook-Pro.local>
2017-01-29 12:50:33 -0500

    changed map overlay to white

--- app/client/components/DevSites/Index/Map/Map.jsx
+++ app/client/components/DevSites/Index/Map/Map.jsx
@@ -159,7 +159,7 @@ export default class Map extends Component {
       'type': 'fill',
       'source': 'guelph-wards',
       'paint': {
-        'fill-color': '#3E6880',
+        'fill-color': '#fff',
         'fill-opacity': 0.3
       }
     });
@@ -178,7 +178,7 @@ export default class Map extends Component {
       'type': 'fill',
       'source': 'wards',
       'paint': {
-        'fill-color': '#3E6880',
+        'fill-color': '#fff',
         'fill-opacity': 0.3
       }
     });
