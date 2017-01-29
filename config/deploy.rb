8a26c7fa6a3671a2f8528b727aec28059ac40481
mtuck063 <meneliktucker@gmail.com>
2017-01-29 12:40:49 -0500

    clean up capistrano code

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,25 +13,38 @@ set :pty, true
 set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 set :resque_environment_task, true
 
-task :npm_install do
-  on roles(:app) do
-    execute "cd #{deploy_to} && npm install --unsafe-perm"
+namespace :git do
+  task :pull do
+    on roles(:app) do
+      execute "cd #{fetch(:deploy_to)} && git pull origin #{fetch(:branch)}"
+    end
   end
 end
-before 'deploy:assets:precompile', 'npm_install'
 
-task :npm_start do
-  on roles(:app) do
-    execute "cd #{deploy_to} && npm start"
+namespace :npm do
+  task :install do
+    on roles(:app) do
+      execute "cd #{fetch(:deploy_to)} && npm install --unsafe-perm"
+    end
+  end
+
+  task :start do
+    on roles(:app) do
+      execute "cd #{fetch(:deploy_to)} && npm start"
+    end
   end
 end
-after 'npm_install', 'npm_start'
 
-task :restart_unicorn do
-  on roles(:web) do
-    execute :service, 'unicorn restart'
+namespace :unicorn do
+  task :restart do
+    on roles(:web) do
+      execute :service, 'unicorn restart'
+    end
   end
 end
-after 'deploy:published', 'restart_unicorn'
 
+after 'git:update', 'git:pull'
+before 'deploy:assets:precompile', 'npm:install'
+after 'npm:install', 'npm:start'
+after 'deploy:published', 'unicorn:restart'
 after 'deploy:published', 'resque:restart'
