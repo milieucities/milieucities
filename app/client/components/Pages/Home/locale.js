395a05074981cb77dee79b882c9d93e950e1ff8b
Dankonino <dankonino@gmail.com>
2017-02-11 18:21:56 -0500

    responsive guelph final

--- app/client/components/Pages/Home/locale.js
+++ app/client/components/Pages/Home/locale.js
@@ -6,8 +6,8 @@ const i18n = new LocalizedStrings({
     signUp: 'Sign Up',
     haveAccount: 'Have an account?',
     signIn: 'Sign In',
-    heroText2: 'Get community insight for urban planning and development projects',
-    launchProject: 'Launch Your Project',
+    heroText2: 'Explore planning and development applications and witness public participation in real-time. Comment, discuss and share ideas with members of the community and city officials. ',
+    launchProject: 'Launch Your Project Today',
     title: 'Discover What is Being Built in Your City',
     featuredDevelopments: 'Featured Developments',
     enterAddress: 'Enter an address',
@@ -15,14 +15,16 @@ const i18n = new LocalizedStrings({
     title1: 'Zoning 101',
     summary1: 'Zoning regulations are the rules of the game if you — or a developer — want to construct a building.',
     title2: 'Whose streets are we planning?',
-    summary2: 'Transportation planning offers huge opportunities to enable equity.'
+    summary2: 'Transportation planning offers huge opportunities to enable equity.',
+    guelphDescription: 'Launching February 22, 2017, Milieu will be deployed city-wide as part of the Guelph Civic Accelerator. Citizens of Guelph can find out what’s being built and participate in the future planning and development of the city. ',
+    guelphPartner: 'Partners'
   },
   fr: {
-    heroText1: 'Obtenez un aperçu communautaire des projets d\'urbanisme et de développement',
+    heroText1: 'pour une meilleure démocratie et la construction de villes axées sur les données.',
     signUp: 'S\'inscrire',
     haveAccount: 'Vous avez une compte?',
     signIn: 'Se connecter',
-    heroText2: 'Rejoignez la communauté pour discuter, contribuer et co-créer un meilleur environnement urbain ensemble',
+    heroText2: 'Explorez les applications de planification et de développement en temps réel. Commentez, discutez et partagez des idées avec les membres de la communauté et les responsables municipaux.',
     launchProject: 'Lancez votre projet aujourd\'hui',
     title: 'Découvrez Ce Qui Est Construit Dans Votre Ville',
     featuredDevelopments: 'Développements en vedette',
@@ -31,7 +33,9 @@ const i18n = new LocalizedStrings({
     title1: 'Zonage 101',
     summary1: 'Les régulations de zonage sont les règles pour quand quelqu’un (tels que vous ou un développeur) veut construire un bâtiment.',
     title2: 'À qui sont les rues que nous planifions ?',
-    summary2: 'La planification des transports offre d’énormes possibilités d’équité.'
+    summary2: 'La planification des transports offre d’énormes possibilités d’équité.',
+    guelphDescription: 'Lancé le 22 février 2017, Milieu sera déployé dans l\'ensemble de la ville dans le cadre de l\'accélérateur civique Guelph. Les citoyens de Geulph pourront découvrir les projects urbains à venir, et participer à la planification et au développement de leur ville.',
+    guelphPartner: 'Les partenaires'
   }
 });
 
