d4ccd795872ee4222cea55b5129a77f61d130163
grrrero <guerrewrong@gmail.com>
2017-07-05 11:42:01 -0400

    Added Noumea page

--- app/client/components/Pages/Entreenord/Entreenord.jsx
+++ app/client/components/Pages/Entreenord/Entreenord.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import css from './entreenord.scss';
+import './slider.less';
+import './tooltip.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tooltip from 'rc-tooltip';
+import Slider from 'rc-slider';
+
+
 
 export default class Entreenord extends Component {
   render() {
     return(
-      <div>
-        <h1>Entreenord</h1>
+      <div className={css.root}>
+        <h2>Un project collaboratif de concertation citoyenne pour la nouvelle entrée nord du centre-ville de Nouméa</h2>
+
+        <h1>À propos du projet de concertation citoyenne:</h1>
+
+        <div>
+      		4 Thématiques
+      	</div>
+
+
       </div>
     )
   }
