8b629814e5077d7f9b5acfecde0fcad3682f63d9
Sharon <sharon.peishan.kennedy@gmail.com>
2017-07-06 01:31:54 -0400

    fixed typo in RadioButtonsWithLabel

--- app/client/components/Common/FormFields/Form.js
+++ app/client/components/Common/FormFields/Form.js
@@ -1,6 +1,6 @@
 import SelectWithLabel from './SelectWithLabel'
 import TextAreaWithLabel from './TextAreaWithLabel'
 import TextInputWithLabel from './TextInputWithLabel'
-import RadioButtonsWithLabel from './RadiobuttonsWithLabel'
+import RadioButtonsWithLabel from './RadioButtonsWithLabel'
 
 export { SelectWithLabel, TextInputWithLabel, TextAreaWithLabel, RadioButtonsWithLabel }
