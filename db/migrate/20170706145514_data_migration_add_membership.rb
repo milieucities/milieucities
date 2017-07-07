43802b7cc5e1c2e30d23ec7b3d68b51c342c46a2
Claire Lesage <clairellesage@gmail.com>
2017-07-07 14:41:04 -0400

    devId and address are searcheable by params

--- db/migrate/20170706145514_data_migration_add_membership.rb
+++
@@ -1,8 +0,0 @@
-class DataMigrationAddMembership < ActiveRecord::Migration
-  def up
-    sample_org = Organization.find_or_create_by(name: "Claire Org")
-    sample_user = User.find_or_create_by(email: "abc@123.com")
-
-    sample_org.users << sample_user
-  end
-end
