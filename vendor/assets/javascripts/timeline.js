db300d849710c3d12de290263137e8d5a79fe323
grrrero <guerrewrong@gmail.com>
2017-07-09 19:35:48 -0400

    Fixed timeline logic.

--- vendor/assets/javascripts/timeline.js
+++ vendor/assets/javascripts/timeline.js
@@ -3,8 +3,10 @@
       
         $el = $(this);
         $el.addClass('tl');
-        if (data == 'Open' || 'Application Complete, Comment Period Open') {
-          var eventTmpl = $('<div class="checkoutwrap"><ul class="checkoutbar"><li class="active"><a href="#">Comment Period</a></li><li class="">Public Meeting</li><li class="">Revision</li><li class="">Decision Meeting</li><li class="">Decision</li></ul>').appendTo($el);
+        if (data == 'Open' || 'Application Received') {
+          var eventTmpl = $('<div class="checkoutwrap"><ul class="checkoutbar"><li class=""><a href="#">Comment Period</a></li><li class="">Public Meeting</li><li class="">Revision</li><li class="">Decision Meeting</li><li class="">Decision</li></ul>').appendTo($el);
+        } else if (data == 'Application Complete, Comment Period Open') {
+          var eventTmpl = $('<div class="checkoutwrap"><ul class="checkoutbar"><li class="active"><a href="#">Comment Period</a></li><li class="active">Public Meeting</li><li class="">Revision</li><li class="">Decision Meeting</li><li class="">Decision</li></ul>').appendTo($el);
         } else if (data == 'Review' || 'Planning Review Stage') {
           var eventTmpl = $('<div class="checkoutwrap"><ul class="checkoutbar"><li class="visited"><a href="#">Comment Period</a></li><li class="active">Public Meeting</li><li class="">Revision</li><li class="">Decision Meeting</li><li class="">Decision</li></ul>').appendTo($el);
         } else if (data == 'Revision') {
