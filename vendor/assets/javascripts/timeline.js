c8976c7c57db2cbd841809deabe3594a4326a94d
grrrero <guerrewrong@gmail.com>
2017-07-09 15:37:15 -0400

    Added timeline.

--- vendor/assets/javascripts/timeline.js
+++ vendor/assets/javascripts/timeline.js
@@ -2,24 +2,19 @@
       return this.each(function() {
       
         $el = $(this);
-        $el.addClass('timeline');
-
-
-        // calc ratio for event positions
-        var ratio = 100/(data.stop_time - data.start_time)
-        
-        $.each(data.lines, function(i,line){
-          var lineTmpl = $('<div class="line"><div class="events"></div></div>').addClass("line "+ line.css).appendTo($el);
-          
-          $.each(line.events, function(index,event){
-            var position = ((event.time - data.start_time)*ratio).toFixed(2);
-
-            var eventTmpl = $('<div class="event"><div class="circle"><div class="circle-inner"></div><div class="label"><text>'+event.title+'</text><time>'+(new Date(event.time).toLocaleString())+'</time></div></div></div>').appendTo($('.events', lineTmpl)).css('left',position+'%');
-          });
-        });
-
-        var timeTmpl = $('<div class="time">').appendTo($el);
-        var periodTmpl = $('<div class="period"><div class="label last">'+(new Date(data.stop_time).toLocaleString())+'</div><div class="label first">'+(new Date(data.start_time).toLocaleString())+'</div></div>').css({left:"0%", width:"100%"}).appendTo(timeTmpl);
-        
+        $el.addClass('tl');
+        if (data == 'Open') {
+          var eventTmpl = $('<div class="checkoutwrap"><ul class="checkoutbar"><li class="active"><a href="#">Comment Period</a></li><li class="">Public Meeting</li><li class="">Revision</li><li class="">Decision Meeting</li><li class="">Decision</li></ul>').appendTo($el);
+          console.log(eventTmpl);
+        } else if (data == 'Review') {
+          var eventTmpl = $('<div class="checkoutwrap"><ul class="checkoutbar"><li class="visited"><a href="#">Comment Period</a></li><li class="active">Public Meeting</li><li class="">Revision</li><li class="">Decision Meeting</li><li class="">Decision</li></ul>').appendTo($el);
+          console.log(eventTmpl);
+        } else if (data == 'Revision') {
+          var eventTmpl = $('<div class="checkoutwrap"><ul class="checkoutbar"><li class="visited"><a href="#">Comment Period</a></li><li class="visited">Public Meeting</li><li class="active">Revision</li><li class="">Decision Meeting</li><li class="">Decision</li></ul>').appendTo($el);
+          console.log(eventTmpl);
+        } else if (data == 'Decision') {
+          var eventTmpl = $('<div class="checkoutwrap"><ul class="checkoutbar"><li class="visited"><a href="#">Comment Period</a></li><li class="visited">Public Meeting</li><li class="visited">Revision</li><li class="active">Decision Meeting</li><li class="">Decision</li></ul>').appendTo($el);
+          console.log(eventTmpl);
+        };
       });
     };
