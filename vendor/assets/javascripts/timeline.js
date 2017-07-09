62823a65c9e0401659ad1e4cb12b54cd989a3a85
grrrero <guerrewrong@gmail.com>
2017-07-09 19:30:00 -0400

    Fixed timeline logic.

--- vendor/assets/javascripts/timeline.js
+++ vendor/assets/javascripts/timeline.js
@@ -3,9 +3,9 @@
       
         $el = $(this);
         $el.addClass('tl');
-        if (data == 'Open') {
+        if (data == 'Open' || 'Application Complete, Comment Period Open') {
           var eventTmpl = $('<div class="checkoutwrap"><ul class="checkoutbar"><li class="active"><a href="#">Comment Period</a></li><li class="">Public Meeting</li><li class="">Revision</li><li class="">Decision Meeting</li><li class="">Decision</li></ul>').appendTo($el);
-        } else if (data == 'Review') {
+        } else if (data == 'Review' || 'Planning Review Stage') {
           var eventTmpl = $('<div class="checkoutwrap"><ul class="checkoutbar"><li class="visited"><a href="#">Comment Period</a></li><li class="active">Public Meeting</li><li class="">Revision</li><li class="">Decision Meeting</li><li class="">Decision</li></ul>').appendTo($el);
         } else if (data == 'Revision') {
           var eventTmpl = $('<div class="checkoutwrap"><ul class="checkoutbar"><li class="visited"><a href="#">Comment Period</a></li><li class="visited">Public Meeting</li><li class="active">Revision</li><li class="">Decision Meeting</li><li class="">Decision</li></ul>').appendTo($el);
