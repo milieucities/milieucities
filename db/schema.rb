a96d600645a844b6d654f5687bce4359faf4186e
Matus Dubrava <dankonino@gmail.com>
2017-07-10 23:32:21 -0400

    adding schema as well, pls review

--- db/schema.rb
+++ db/schema.rb
@@ -185,8 +185,10 @@ ActiveRecord::Schema.define(version: 20170709173035) do
   add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
 
   create_table "meetings", force: :cascade do |t|
+    t.string   "title"
     t.string   "meeting_type"
-    t.datetime "time"
+    t.string   "time"
+    t.datetime "date"
     t.string   "location"
     t.integer  "dev_site_id"
     t.datetime "created_at",   null: false
