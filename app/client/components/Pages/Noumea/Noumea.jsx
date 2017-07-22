587d451df2ca9ccf20baf5b4f652498492dc37ab
luluissaa <luisa.lyji@gmail.com>
2017-07-22 15:36:28 -0400

    fixed top logos and introtext for all screen sizes

--- app/client/components/Pages/Noumea/Noumea.jsx
+++ app/client/components/Pages/Noumea/Noumea.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import css from './css/noumea.scss'
+import Header from './Header'
+import MobileFooter from './MobileFooter'
 import { debounce, uniqueId } from 'lodash'
 
 export default class Noumea extends Component {
@@ -17,34 +19,38 @@ export default class Noumea extends Component {
   }
 
   render() {
-    const { loading } = this.state;
+    const {  isMobile } = this.state;
 
     return (
       <div className={css.container}>
+        { !isMobile &&
+          <Header />
+        }
 
-        <div className= {css.logoContainer}>
-            <img
-              src={require(`./images/logo-noumea-m.jpg`)}
-            />
-            <img
-              src={require(`./images/logo-provincesud.jpg`)}
-            />
-            <img
-              src={require(`./images/logo-nc.png`)}
-            />
-      </div>
 
-      <div className={css.titleContainer}>
+          <div className="row">
+            <div className= {css.logoContainer}>
+                <img
+                  src={require(`./images/logo-noumea.jpg`)}
+                />
+                <img
+                  src={require(`./images/logo-provincesud.png`)}
+                />
+                <img
+                  src={require(`./images/logo-nc.png`)}
+                />
+          </div>
+
+          </div>
+
+      <div className={css.introContainer}>
         <div className={css.description}>
-            <h1>ENTRÉE NORD</h1>
-  <p>Un projet collaboratif de concertation citoyenne pour la nouvelle entrée nord du centre-ville de Nouméa</p>
-        </div>
-
-  <div className={css.videoContainer}>
-    <iframe src="https://www.youtube.com/embed/cBkWhkAZ9ds?rel=0&amp;showinfo=0" allowfullscreen></iframe>
-  </div>
+              <h1>ENTRÉE NORD</h1>
+              <p>Un projet collaboratif de concertation citoyenne pour la nouvelle entrée nord du centre-ville de Nouméa</p>
+          </div>
+      <div className={css.videoContainer}>
+        <iframe src="https://www.youtube.com/embed/cBkWhkAZ9ds?rel=0&amp;showinfo=0" allowfullscreen></iframe>
       </div>
-
 
       <div className={css.visionContainer}>
       <h2>VISION POUR L'ENTRÉE NORD</h2>
@@ -63,48 +69,48 @@ export default class Noumea extends Component {
       </p>
     </div>
 
-<div className={css.themeContainer}>
+    <div className={css.aboutContainer}>
+      <div className={css.themeContainer}>
 
-  <p>4 thematiques</p>
+        <p>4 thematiques</p>
 
-</div>
+      </div>
 
-<div className={css.timelineContainer}>
-  <h1>À propos du projet de concertation citoyenne</h1>
-    <img
-      src={require(`./images/timeline.svg`)}
-    />
+      <div>
 
-  <p>
-    Votre participation peut influencer les futurs aménagements de votre ville, et en l'occurence la vision urbaine de l'entrée nord. La période de concertation grâce à ce site web se déroulera du 24 Juillet à fin Octobre.
-  </p>
+        <div className={css.timelineContainer}>
+          <h1>À propos du projet de concertation citoyenne</h1>
+            <img
+              src={require(`./images/timeline.svg`)}
+            />
+                <p ClassName={css.bubble}>
+                  Votre participation peut influencer les futurs aménagements de votre ville, et en l'occurence la vision urbaine de l'entrée nord. La période de concertation grâce à ce site web se déroulera du 24 Juillet à fin Octobre.
+                </p>
+                <p ClassName={css.bubble}>
+                  L'engagement civique est crucial. Vous pouvez donner votre avis sur l'avenir de votre ville durant des consultations publiques ou bien en utilisant ce site web initié par la municipalité de Nouméa.
+                </p>
+                <p ClassName={css.bubble}>
+                  Le calendrier de l'exposition sera bientôt affiché. Vous pourrez y laisser votre avis, qui sera immédiatement transmis au site web.
+                </p>
 
-  <p>
-    L'engagement civique est crucial. Vous pouvez donner votre avis sur l'avenir de votre ville durant des consultations publiques ou bien en utilisant ce site web initié par la municipalité de Nouméa.
-  </p>
-
-  <p>
-    Le calendrier de l'exposition sera bientôt affiché. Vous pourrez y laisser votre avis, qui sera immédiatement transmis au site web.
-  </p>
-</div>
-
-
-<div className={css.partnersContainer} >
-
-  <h2>les partenaires</h2>
-    <img
-      src={require(`./images/logo-acp.svg`)}
-    />
-    <img
-      src={require(`./images/logo-scet.png`)}
-
-    />
-    <img
-      src={require(`./images/logo-milieu.png`)}
-
-    />
-</div>
-
+    </div>
+      </div>
+    </div>
+          </div>
+          <div className="row">
+            <div className={css.partnersContainer} >
+              <h2>les partenaires</h2>
+                <img
+                  src={require(`./images/logo-acp.svg`)}
+                />
+                <img
+                  src={require(`./images/logo-scet.png`)}
+                />
+                <img
+                  src={require(`./images/logo-milieu.png`)}
+                />
+            </div>
+          </div>
 
   </div>
     )
