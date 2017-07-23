83e9cb7899b127b91333f6336f48d597e81fbecc
luluissaa <luisa.lyji@gmail.com>
2017-07-23 18:02:20 -0400

    screen smaller than 720 is alright looking

--- app/client/components/Pages/Noumea/Noumea.jsx
+++ app/client/components/Pages/Noumea/Noumea.jsx
@@ -54,27 +54,27 @@ export default class Noumea extends Component {
 
       <div className={css.introContainer}>
       <div className={css.videoContainer}>
-        <iframe src="https://www.youtube.com/embed/cBkWhkAZ9ds?rel=0&amp;showinfo=0" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/cBkWhkAZ9ds?rel=0&amp;showinfo=0" allowFullScreen></iframe>
       </div>
 
       <div className={css.visionContainer}>
-      <h2>VISION POUR L'ENTRÉE NORD</h2>
-      <h3>Proposition de scénario d'aménagement</h3>
-      <p>
-        L’entrée nord du centre ville de Nouméa est une zone caractérisée par l'ancien Centre Hospitalier Territorial Gaston-Bourret, son parking et la voie express #1. C’est actuellement un no man’s land, coupé du littoral et du centre ville pourtant si proches, qui marque la baie de la Grande Rade d'un aspect désertique, vide et fantôme... Une baie jusque là délaissée, alors que son panorama est la première image de Nouméa que perçoivent la plupart des visiteurs. Une baie ignorée alors qu’elle est adjacente à deux sources majeurs de l’économie néo-caledonienne: la Société Le Nickel et le Port Autonome de la Nouvelle Calédonie.
-      </p>
-      <p>
-        Et si cela changeait? Quel serait l'impact sur l'entrée nord et ses environs immédiats? Sur Nouméa et ses habitants? Enfin, sur  la Nouvelle-Calédonie?
-      </p>
-      <p>
-      Nous proposons de developper une vision partagée pour l’entrée nord en tant que passerelle autant physique que symbolique: reliant le coeur de ville à la mer, les croisiéristes et touristes à la culture néo-calédonienne, et les populations nouméennes entre elles.
-      </p>
-      <p>
-        Ces efforts seront menés par l’architecte urbaniste Aziza Chaouni, accompagnée de la SCET et de toutes les parties prenantes concernées par le projet.
-      </p>
-    </div>
+        <div className={css.introText}>
+          <h2>VISION POUR L'ENTRÉE NORD</h2>
+          <h3>Proposition de scénario d'aménagement</h3>
+          <p>
+            L’entrée nord du centre ville de Nouméa est une zone caractérisée par l'ancien Centre Hospitalier Territorial Gaston-Bourret, son parking et la voie express #1. C’est actuellement un no man’s land, coupé du littoral et du centre ville pourtant si proches, qui marque la baie de la Grande Rade d'un aspect désertique, vide et fantôme... Une baie jusque là délaissée, alors que son panorama est la première image de Nouméa que perçoivent la plupart des visiteurs. Une baie ignorée alors qu’elle est adjacente à deux sources majeurs de l’économie néo-caledonienne: la Société Le Nickel et le Port Autonome de la Nouvelle Calédonie.
+          </p>
+          <p>
+            Et si cela changeait? Quel serait l'impact sur l'entrée nord et ses environs immédiats? Sur Nouméa et ses habitants? Enfin, sur  la Nouvelle-Calédonie?
+          </p>
+          <p>
+          Nous proposons de developper une vision partagée pour l’entrée nord en tant que passerelle autant physique que symbolique: reliant le coeur de ville à la mer, les croisiéristes et touristes à la culture néo-calédonienne, et les populations nouméennes entre elles.
+          </p>
+          <p>
+            Ces efforts seront menés par l’architecte urbaniste Aziza Chaouni, accompagnée de la SCET et de toutes les parties prenantes concernées par le projet.
+          </p>
+        </div>
 
-    <div className={css.aboutContainer}>
       <div className={css.themeContainer}>
           <div className={css.theme}>
             <h2>Thématiques</h2>
@@ -89,21 +89,24 @@ export default class Noumea extends Component {
               />
           </div>
       </div>
+    </div>
+
+
+    <div className={css.aboutContainer}>
+      <h2>À propos du projet de concertation citoyenne</h2>
+        <div>
+            <p>
+              Votre participation peut influencer les futurs aménagements de votre ville, et en l'occurence la vision urbaine de l'entrée nord. La période de concertation grâce à ce site web se déroulera du 31 Juillet à fin Octobre.
+            </p>
         <div className={css.timelineContainer}>
-          <h2>À propos du projet de concertation citoyenne</h2>
-            <div>
-                <p>
-                  Votre participation peut influencer les futurs aménagements de votre ville, et en l'occurence la vision urbaine de l'entrée nord. La période de concertation grâce à ce site web se déroulera du 31 Juillet à fin Octobre.
-                </p>
             </div>
             <img
               src={require(`./images/timeline.svg`)} alt="L'engagement civique est crucial. Vous pouvez donner votre avis sur l'avenir de votre ville durant des consultations publiques ou bien en utilisant ce site web initié par la municipalité de Nouméa."
             />
-                <p>
-                  Le calendrier de l'exposition sera bientôt affiché. Vous pourrez y laisser votre avis, qui sera immédiatement transmis au site web.
-                </p>
-
     </div>
+            <p>
+              Le calendrier de l'exposition sera bientôt affiché. Vous pourrez y laisser votre avis, qui sera immédiatement transmis au site web.
+            </p>
       </div>
           </div>
 
@@ -115,14 +118,15 @@ export default class Noumea extends Component {
                     src={require(`./images/logo-acp.svg`)}
                   /></a>
 
-                <a href="https://www.noumea.nc">
+                <a href="http://www.caissedesdepots.fr/">
               <img
                     src={require(`./images/logo-scet.png`)}
                   />
               </a>
-                  <img
-                    src={require(`./images/logo-milieu.png`)}
-                  />
+              <a href="https://milieu.io/"><img
+                src={require(`./images/logo-milieu.png`)}
+              /></a>
+
             </div>
 
               </div>
