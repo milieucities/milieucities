88499355937be1013384183fe91aab4aa9bf454c
Matus Dubrava <dankonino@gmail.com>
2017-07-24 18:57:05 -0400

    option for puma restart

--- config/puma.rb
+++ config/puma.rb
@@ -8,6 +8,9 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
+
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
