7d9c1cd01efe7514e4cbcff51ca84d3e68fbc068
Matus Dubrava <dankonino@gmail.com>
2017-07-24 23:13:42 -0400

    correct path to build

--- app/client/webpack.prod.config.js
+++ app/client/webpack.prod.config.js
@@ -17,8 +17,8 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, "app"),
-    filename: "main.js",
+    path: path.resolve(__dirname, 'build'),
+    filename: "[name].js",
     publicPath: '/public/'
   },
 
