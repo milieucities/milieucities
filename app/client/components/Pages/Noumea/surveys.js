468fa94e92e52c296fa1e80f8ca9475c1d4ebfa3
Zachary Levy <zacharyalevy@gmail.com>
2017-07-28 15:51:19 -0400

    surveys in js

---
+++ app/client/components/Pages/Noumea/surveys.js
@@ -0,0 +1,221 @@
+export const surveys = [
+  {
+    "id": 1,
+    "title": "1.1 Usage temporaire de l'ancien complexe Gaston-Bourret",
+    "description": "L’ancien site de l’hôpital Gaston-Bourret est composé de 26 bâtiments,\
+      dont trois sont voués à la démolition, et un, en préfabriqué, au démantèlement.\
+       Nous proposons que les bâtiments à haute valeur patrimoniale ne soit pas utilisés durant la phase temporaire,\
+       durant laquelle ils subiront une expertise à des fins de restauration.\
+       A l’opposé, les autres bâtiments recevront  selon leur état des programmes adéquats .",
+    "map": "./images/survey-map-immediat-1.png",
+    "note": "Notez les differents programmes que nous proposons!",
+    "questions": [
+      {
+        "id": 1,
+        "text": "RDC espace public ouvert polyvalent (murs et cloisons retirés) Étages: espaces educatifs"
+      },{
+        "id": 2,
+        "text": "Bureaux co-working"
+      },{
+        "id": 3,
+        "text": "Commerces"
+      },{
+        "id": 4,
+        "text": "Espace à usage pluriel"
+      },{
+        "id": 5,
+        "text": "Friche artistique-studios"
+      },{
+        "id": 6,
+        "text": "Espace recherche scientifique"
+      },{
+        "id": 7,
+        "text": "Café"
+      },{
+        "id": 8,
+        "text": "Bureaux"
+      },{
+        "id": 9,
+        "text": "Espaces verts et jardins potagers"
+      },{
+        "id": 10,
+        "text": "Bâtiments fermés au public pour réhabilitation"
+      },{
+        "id": 11,
+        "text": "Maison du projet (accueil/ exposition)"
+      }
+    ]
+  }, {
+    "id": 2,
+    "title": "1.2 Usage temporaire du parking et du terrain vague adjacent",
+    "description": "Coincé entre le mur de soutènement de l’ancien hôpital et la voie express 1, le parking Gaston-Bourret est peu utilisé.\
+       Son manque d’ombrage et de verdure ainsi que sa surface en asphalte qui capte la chaleur, le rendent peu hospitalier. Cependant, grâce à divers mobiliers urbains, à des auvents, \
+       à l’introduction de végétation et à l’impulse d’événements culturels ponctuels, le parking peut devenir très rapidement non seulement un espace publique à l’excellent\
+       confort thermique, mais aussi un lieu dynamique où diverses cultures urbaines peuvent de rencontrer et s’exprimer.  Nous proposons de placer, de manière provisoire, \
+       le terminus de bus (réseau RAI et bus Karai) présentement localisé sur le site du Port Moselle, ainsi qu'un parking sur le terrain vague à l’ouest du parking de l’ancien CHT Gaston-Bourret.",
+    "map": "./images/survey-map-immediat-2.png",
+    "note": "Notez les differents programmes que nous proposons! Donnez nous votre avis sur chacun!",
+    "questions": [
+      {
+        "id": 1,
+        "text": "Jardinières mobiles"
+      },{
+        "id": 2,
+        "text": "Stands restauration rapide / stand glacier"
+      },{
+        "id": 3,
+        "text": "Espace camionnettes restauration - Food truck"
+      },{
+        "id": 4,
+        "text": "Aire de jeux enfants"
+      },{
+        "id": 5,
+        "text": "Parasols géants"
+      },{
+        "id": 6,
+        "text": "Espace skateboard"
+      },{
+        "id": 7,
+        "text": "Mur végétal anti-bruit"
+      },{
+        "id": 8,
+        "text": "Surfaces peintes toiles géantes pour artistes"
+      },{
+        "id": 9,
+        "text": "Zones vertes créées par percement de l’asphalte et zones pique-nique"
+      },{
+        "id": 10,
+        "text": "Espace marchés éphémères (parvis + tables)"
+      },{
+        "id": 11,
+        "text": "Jardins potagers dans jardinières mobiles"
+      },{
+        "id": 12,
+        "text": "Trellis avec hamacs"
+      },{
+        "id": 13,
+        "text": "Mobilier urbain reconfigurable"
+      }
+    ]
+  }, {
+    "id": 3,
+    "title": "Circulations",
+    "description": "La voie express 1 et la voie camion SLN longeant le front de mer contribuent à couper l’entrée nord u littoral et de la Grande Rade.\
+      Cette coupure est accentuée par le fait que le complexe hospitalier est surélevé de 5 m par rapport au niveau de l’entrée nord, ses murs de soutènement au \
+      est et ouest, empêchant tout accès piéton.\
+      Afin de remédier à l’isolement de l’entrée nord nous avons proposé trois strategies principales:\
+      > Le déplacement de la voie express #1 vers le périmètre nord de l’ancien complexe hospitalier. \
+      > Le déplacement de la voie camion SLN sur la voie express #1 selon un horaire prédéfini (hors heure de pointe)\
+      > La création d’une large passerelle végétalisée qui mènerait de l’ancien complexe hospitalier vers le nouveau front de mer et son parc botanique. \
+      > Le renforcement de l’axe piéton nord-sud menant de la place des Cocotiers vers le front de mer. ",
+    "map": "./images/survey-map-circulations.png",
+    "note": "Notez les differents modes de circulations que nous proposons + donnez nous votre avis sur chacun!",
+    "questions": [
+      {
+        "id": 1,
+        "text": "Voie express #1 déplacée"
+      },{
+        "id": 2,
+        "text": "Voie Néobus retracée et réaménagée + stations"
+      },{
+        "id": 3,
+        "text": "Voie Karai bus + stations"
+      },{
+        "id": 4,
+        "text": "Voie bus + station SMTI"
+      },{
+        "id": 5,
+        "text": "Voie tchou-tchou train"
+      },{
+        "id": 6,
+        "text": "Voie cyclable + Vélib"
+      },{
+        "id": 7,
+        "text": "Voie piétonne"
+      },{
+        "id": 8,
+        "text": "Connections piétonne"
+      },{
+        "id": 9,
+        "text": "Passerelle végétalisée principale"
+      },{
+        "id": 10,
+        "text": "Parking verticaux vertes (P.V.V.)"
+      }
+    ]
+  }, {
+    "id": 4,
+    "title": "Espaces publiques/ espaces verts",
+    "description": "Notre vision pour l’entrée nord à l’échelle du centre ville et de ses environs urbains vise à la désenclaver et à l’ouvrir non\
+      seulement vers le littoral mais aussi vers quatre espaces publiques clefs, desquelles elle deviendra le coeur battant.\
+      En effet, l’entrée nord se trouve à la croisée de 4 espaces publiques majeurs du centre ville: un existant, la place des Cocotiers, un en cours de \
+      développement, le Quai Ferry, et deux que deux nouveaux espaces verts que nous souhaitons créer:\
+      Le parc en terrasse SLN, qui comprendra des aires de loisirs passifs, une voie cyclables, des allées piétonnes, \
+      un parking en surface vert et durable, le terminal ferry et un nouveau musée maritime avec \
+      Le parc linéaire avenue James Cook Nous proposons que l’entrée nord devienne le fil conducteur reliant ces\
+      4 espaces publiques. Pour ce faire nous souhaitons lui donner une identité unique: celle de jardin botanique vivant\
+      représentant les diverses essences végétales et agricoles caractéristiques des communautés qui ont fait la richesse culturelle de la Nouvelle Calédonie. \
+      Ces communautés seront amenées à participer à la création et au développement continu de ce parc botanique. ",
+    "map": "./images/survey-map-public.png",
+    "note": "3.1 Que pensez vous du placement et des identités de chaque espace vert/espace publique que nous proposons?",
+    "questions": [
+      {
+        "id": 1,
+        "text": "Bâtiment multifonctionnel (École de danse, bureaux, hôtel, ...)"
+      },{
+        "id": 2,
+        "text": "Résidentiel avec commerce au RDC"
+      },{
+        "id": 3,
+        "text": "Parking verticaux vertes (avec commerce, sports ou culture)"
+      },{
+        "id": 4,
+        "text": "Bureaux"
+      },{
+        "id": 5,
+        "text": "Espaces à usage collectif"
+      },{
+        "id": 6,
+        "text": "Terminal croisiéristes"
+      },{
+        "id": 7,
+        "text": "Musée Maritime de Nouvelle-Calédonie avec son pavillon flottant"
+      }
+    ]
+  }, {
+    "id": 5,
+    "title": "Bâti",
+    "description": "Une fois l’entrée nord re-aménagée, de manière temporaire puis de manière définitive, la partie nord du centre ville s’en verra\
+      totalement transformée et deviendra un lieu de residence et de loisir prisé. Donc, l’aménagement des enveloppes et des programmations du bâtit doit être \
+      calibré avec finesse afin de mettre en valeur et rehausser les espaces publiques et les espaces verts proposés. Le bâtit doit aussi contribuer à établir une \
+      nouvelle identité de la ville de Nouméa, dont la facade avant et le skyline sont actuellement dominés par la tour Beef. Enfin, le bâtit doit être pensé de manière\
+      bioclimatique afin qu’il contribue au confort thermique autant de ses alentours que de ses intérieurs, en privilégiant les techniques de ventilation et de déshumidication passives.\
+      Ainsi, nous avons proposé des bâtiments nouveaux ainsi que des bâtiments réhabilités.",
+    "map": "./images/survey-map-bati.png",
+    "note": "Que pensez vous de la programmation des espaces bâtits que nous proposons?",
+    "questions": [
+      {
+        "id": 1,
+        "text": "Bâtiment multifonctionnel (École de danse, bureaux, hôtel, ...)"
+      },{
+        "id": 2,
+        "text": "Résidentiel avec commerce au RDC"
+      },{
+        "id": 3,
+        "text": "Parking verticaux vertes (avec commerce, sports ou culture)"
+      },{
+        "id": 4,
+        "text": "Bureaux"
+      },{
+        "id": 5,
+        "text": "Espaces à usage collectif"
+      }, {
+        "id": 6,
+        "text": "Terminal croisiéristes"
+      },{
+        "id": 7,
+        "text": "Musée Maritime de Nouvelle-Calédonie avec son pavillon flottant"
+      }
+    ]
+  }
+]
