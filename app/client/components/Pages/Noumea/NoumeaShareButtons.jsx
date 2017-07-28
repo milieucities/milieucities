eca1471a467951a9b8aeec031c93211c4ea39e8d
Jaimin <jaimin.d.88@gmail.com>
2017-07-28 16:28:02 -0400

    changed formatting

--- app/client/components/Pages/Noumea/NoumeaShareButtons.jsx
+++ app/client/components/Pages/Noumea/NoumeaShareButtons.jsx
@@ -8,44 +8,44 @@ const TwitterIcon = generateShareIcon('twitter');
 const NoumeaShareButtons = ({isMobile}) => {
 
     return (
-        <div className="row">
-          <div className="col-xs-3 col-md-4 icons">
-            { isMobile &&
-              <img
-                src={require(`./images/theme-immediat.svg`)}
-                width="45px"
-                height="45px"
-             />
-            }
-            {
-              !isMobile &&
-              <img
-                src={require(`./images/theme-immediat.svg`)}
-                width="85px"
-                height="85px"
-             />
-            }
-            <FacebookShareButton
-              url="/participez"
-              title="Un projet collaboratif de concertation citoyenne pour la nouvelle entrée nord du centre-ville de Nouméa"
-              media={require(`./images/theme-immediat.svg`)}
-            >
-              <FacebookIcon size={32} round />
-            </FacebookShareButton>
-            
-            <TwitterShareButton
-              url="/participez"
-              title="Un projet collaboratif de concertation citoyenne pour la nouvelle entrée nord du centre-ville de Nouméa"
-              media={require(`./images/theme-immediat.svg`)}
-            >
-              <TwitterIcon size={32} round />
-            </TwitterShareButton>
-            
-            <a href="mailto:webmaster@example.com">
-              <i className="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
-            </a>
-          </div>
+      <div className="row">
+        <div className="col-xs-3 col-md-4 icons">
+          { isMobile &&
+            <img
+              src={require(`./images/theme-immediat.svg`)}
+              width="45px"
+              height="45px"
+            />
+          }
+          {
+            !isMobile &&
+            <img
+              src={require(`./images/theme-immediat.svg`)}
+              width="85px"
+              height="85px"
+            />
+          }
+          <FacebookShareButton
+            url="/participez"
+            title="Un projet collaboratif de concertation citoyenne pour la nouvelle entrée nord du centre-ville de Nouméa"
+            media={require(`./images/theme-immediat.svg`)}
+          >
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+          
+          <TwitterShareButton
+            url="/participez"
+            title="Un projet collaboratif de concertation citoyenne pour la nouvelle entrée nord du centre-ville de Nouméa"
+            media={require(`./images/theme-immediat.svg`)}
+          >
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+          
+          <a href="mailto:webmaster@example.com">
+            <i className="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
+          </a>
         </div>
+      </div>
     )
 }
 
