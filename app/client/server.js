081eaa90a7c6d3e69a27f246d3b4107a6c9fff39
Matus Dubrava <dankonino@gmail.com>
2017-07-29 15:59:10 -0400

    keep it simple

--- app/client/server.js
+++ app/client/server.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 
 var app = express();
 
-var static_path = path.join(__dirname, '/build');
+var static_path = path.join(__dirname, './build');
 
 app.enable('trust proxy');
 
