376fc44ac063449107ad997b69dcd282bbd768a0
Matus Dubrava <dankonino@gmail.com>
2017-07-29 18:34:21 -0400

    added heroku as listener with HOST=0.0.0.0 and fall back for local env

--- app/client/server.js
+++ app/client/server.js
@@ -11,4 +11,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 //  so that you can access your site with https default port.
 // Falback port will be 8080; basically for pre-production test in localhost
 // You will use $ npm run prod for this
-app.listen(8099);
+app.listen(process.env.PORT || 8080);
