1700f68910bafbef2f4ace2e80a1b57a00e4eed5
Sharon <sharon.peishan.kennedy@gmail.com>
2017-09-10 17:51:00 -0400

    removed DedupePlugin

--- app/client/webpack.dev.config.js
+++ app/client/webpack.dev.config.js
@@ -27,7 +27,6 @@ module.exports = {
     //     'NODE_ENV': JSON.stringify('production')
     //   }
     // }),
-    new webpack.optimize.DedupePlugin(), //dedupe similar code
     new webpack.optimize.UglifyJsPlugin(), //minify everything
     new webpack.optimize.AggressiveMergingPlugin() //Merge chunks
   ],
